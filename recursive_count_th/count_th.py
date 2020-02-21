1b95af2e62289932017499f7110b5b6ebaf33635
Ayomide Jones <ayormeday@gmail.com>
2020-02-21 10:50:09 +0100

    recursive count task completed and tested

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,16 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
+    # TBC      
+    # Base Case to check if no word
+
+    if (len(word) == 0): 
+        return 0 
     
-    pass
+    # check if the first two letters in the word contains 'th' compare the next pair recursively
+    elif word[:2] == "th":
+        return count_th(word[1:]) + 1
+
+    # continue recursively till base case is evoked.
+    else: 
+        return count_th(word[1:])
